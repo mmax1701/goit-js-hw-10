14273c112aa0d55ff14606f7b671ce4496eeff2a
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-06-13 21:40:22 +0300

    hw10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,35 @@
+const API_KEY = 'live_VbFV4WbGs9A1XvgFEwcgCUcBowUq4LGh14GSiDpXv7fiSHQrSxsaAiJvCrzBxyXJ';
+
 export function fetchBreeds() {
-    return fetch('https://api.thecatapi.com/v1/breeds', {
-        headers: {
-            'x-api-key': 'live_VbFV4WbGs9A1XvgFEwcgCUcBowUq4LGh14GSiDpXv7fiSHQrSxsaAiJvCrzBxyXJ',
-        }
+  return fetch('https://api.thecatapi.com/v1/breeds', {
+    headers: {
+      'x-api-key': API_KEY
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch breed information');
+      }
+      return response.json();
     })
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => {
-            throw error;
-        });
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
 
 export function fetchCatByBreed(breedId) {
-    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-    return fetch(url, {
+  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
     headers: {
-      'x-api-key': 'live_VbFV4WbGs9A1XvgFEwcgCUcBowUq4LGh14GSiDpXv7fiSHQrSxsaAiJvCrzBxyXJ',
-    },
+      'x-api-key': API_KEY
+    }
   })
-    .then(response => response.json())
-    .then(data => data)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch cat information');
+      }
+      return response.json();
+    })
     .catch(error => {
-      throw error;
+      throw new Error(error.message);
     });
 }
--- src/index.js
+++ src/index.js
@@ -4,3 +4,93 @@ const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function showError() {
+  error.style.display = 'block';
+}
+
+function hideError() {
+  error.style.display = 'none';
+}
+
+function showCatInfo(imageUrl, breedName, description, temperament) {
+  const catImage = document.createElement('img');
+  catImage.src = imageUrl;
+
+  const nameElement = document.createElement('p');
+  nameElement.textContent = `${breedName}`;
+  nameElement.classList.add('cat-name');
+  nameElement.style.fontWeight = 'bold';
+  nameElement.style.fontSize = '20px';
+
+  const descriptionElement = document.createElement('p');
+  descriptionElement.textContent = `Description: ${description}`;
+
+  const temperamentElement = document.createElement('p');
+  temperamentElement.innerHTML = `<span class="temperament">Temperament:</span> ${temperament}`;
+  const temperamentSpan = temperamentElement.querySelector('.temperament');
+  temperamentSpan.style.fontWeight = 'bold';
+
+
+  catInfo.innerHTML = '';
+  catInfo.appendChild(catImage);
+  catInfo.appendChild(nameElement);
+  catInfo.appendChild(descriptionElement);
+  catInfo.appendChild(temperamentElement);
+  catInfo.style.display = 'block';
+}
+
+function hideCatInfo() {
+  catInfo.style.display = 'none';
+}
+
+breedSelect.addEventListener('change', () => {
+  const selectedBreedId = breedSelect.value;
+  
+  hideCatInfo();
+  showLoader();
+  hideError();
+
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      const breed = data[0].breeds[0];
+      const imageUrl = data[0].url;
+
+      showCatInfo(imageUrl, breed.name, breed.description, breed.temperament);
+    })
+    .catch(error => {
+      showError();
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+});
+
+showLoader();
+hideError();
+
+fetchBreeds()
+  .then(data => {
+    data.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+  })
+  .catch(error => {
+    showError();
+    console.error(error);
+  })
+  .finally(() => {
+    hideLoader();
+  });
